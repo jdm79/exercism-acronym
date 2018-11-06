02a29ee3f754c681eb0107764258e9de46ad23a0
jdm79 <james.malvern@gmail.com>
2018-11-06 13:02:59 +0000

    comment out old, convoluted solution

--- acronym.rb
+++ acronym.rb
@@ -2,16 +2,16 @@ module BookKeeping
   VERSION = 4
 end
 
-class Acronym
-  def self.abbreviate(phrase)
-    result = []
-    words = phrase.sub(/[-]/, ' ').split()
-    words.each do |word|
-      result.push(word[0,1].capitalize)
-    end
-    result.join
-  end
-end
+# class Acronym
+#   def self.abbreviate(phrase)
+#     result = []
+#     words = phrase.sub(/[-]/, ' ').split()
+#     words.each do |word|
+#       result.push(word[0,1].capitalize)
+#     end
+#     result.join
+#   end
+# end
 
 # Mentor Feedback:
 # Hello!
