0381f6c2210870e930d830e569aba08dd57d7bc6
jdm79 <james.malvern@gmail.com>
2018-11-06 11:07:14 +0000

    tests scan to catch word boundaries

--- acronym.rb
+++ acronym.rb
@@ -12,3 +12,23 @@ class Acronym
     result.join
   end
 end
+
+# Mentor Feedback:
+# Hello!
+# Using split looks like an obvious choice (I agree!), 
+# but that's the catch in this exercise. With split, 
+# you need to focus on the delimiters (space and - ).
+
+# Ruby has also String#scan. With scan and a regex, 
+# you can focus on the parts you need. For instance, 
+# scan with the regex /\b\w/ will catch 'word boundaries' 
+# (space, - and more) followed by a word character.
+# Thus returning the first letters only.
+# I use Rubular for my regular expressions :)
+# I look forward to seeing your next iteration!
+
+class Acronym
+  def self.abbreviate(phrase)
+    phrase.scan(/\b\w/)
+  end
+end
